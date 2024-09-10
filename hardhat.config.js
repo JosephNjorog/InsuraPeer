3e6c4f673f802e12269e257e07a9a335360c13c4
JosephNjorog <njorojoe11173@gmail.com>
2024-09-10 03:34:27 +0300

    Refactor file structure and update dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,27 @@
-require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
 
 module.exports = {
-  solidity: '0.8.4',
   networks: {
-    hardhat: {},
     base: {
-      url: 'https://base-goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID',
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: "https://base-goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: [`0xfeb7fc04d20f2d2ee36f49313c89d6ef60fb50f53be0e4033ba0363946f7cbbd`]
     },
     optimism: {
-      url: 'https://optimism-goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID',
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      url: "https://optimism-goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      accounts: [`0xfeb7fc04d20f2d2ee36f49313c89d6ef60fb50f53be0e4033ba0363946f7cbbd`]
     },
   },
   etherscan: {
-    apiKey: 'YOUR_ETHERSCAN_API_KEY',
+    apiKey: "your_etherscan_api_key"
   },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
