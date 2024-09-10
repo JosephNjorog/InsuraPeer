e6095724c4b4d979840a1cc51f3b9765807a5d64
JosephNjorog <njorojoe11173@gmail.com>
2024-09-10 17:52:42 +0300

    Update dependencies in package.json

--- insurenet-frontend/src/App.js
+++ insurenet-frontend/src/App.js
@@ -6,6 +6,10 @@ import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 import GroupDashboard from './pages/GroupDashboard';
 import Services from './components/Services'
+import SubmitClaims from './pages/SubmitClaims.jsx'
+import PeerReviewPage from './pages/PeerReviewPage.jsx'
+import GroupVotingPage from './pages/GroupVoting.jsx'
+import GroupInsurancePlanCustomization from './pages/GroupInsurancePlanCustomization.jsx'
 
 function App() {
   return (
